cffa941896b829e4d5d9e266ed78a67a9eafd3fb
Alex <agscheaua@gmail.com>
2025-12-31 12:08:25 +0200

    i need to modify the css to look more pleasing and to change the js logic
    i need to show all element at the start of the
    game

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
 
-
 let containerChoices = document.createElement("div");
+containerChoices.classList.add("containerChoices") 
 document.body.appendChild(containerChoices);
 
 let buttonRock = document.createElement("button");
 buttonRock.textContent = "ROCK";
 buttonRock.classList.add("choice", "rock");
+containerChoices.appendChild(buttonRock);
 
 let buttonPaper = document.createElement("button");
 buttonPaper.textContent = "PAPER";
 buttonPaper.classList.add("choice", "paper");
+containerChoices.appendChild(buttonPaper);
 
 let buttonScissor = document.createElement("button");
 buttonScissor.textContent = "SCISSOR";
 buttonScissor.classList.add("choice", "scissor");
-
-containerChoices.appendChild(buttonRock);
-containerChoices.appendChild(buttonPaper);
 containerChoices.appendChild(buttonScissor);
 
 let scoreBoard = document.createElement("div");
@@ -25,9 +24,22 @@ scoreBoard.classList.add("scoreBoard");
 document.body.appendChild(scoreBoard);
 
 let humanScoreBoard = document.createElement("span");
-let computerScoreBoard = document. createElement("span")
 scoreBoard.appendChild(humanScoreBoard);
+let computerScoreBoard = document. createElement("span");
 scoreBoard.appendChild(computerScoreBoard);
+
+let choiceTaken = document.createElement("div");
+choiceTaken.classList.add("choiceTaken");
+document.body.insertBefore(choiceTaken, scoreBoard);
+
+let humanChoiceTaken = document.createElement("span")
+choiceTaken.appendChild(humanChoiceTaken);
+let computerChoiceTaken = document.createElement("span");
+choiceTaken.appendChild(computerChoiceTaken);
+
+let gameInfo = document.createElement("div");
+gameInfo.classList.add("gameInfo");
+document.body.insertBefore(gameInfo, choiceTaken);
 
 let humanScore = 0; 
 let computerScore = 0;
@@ -38,93 +50,78 @@ containerChoices.addEventListener("click", (event) => {
     let calcNumberChoice = (Math.floor(Math.random() * 100 + 1)); 
     let computerChoice = getComputerChoice(calcNumberChoice);
 
+    humanChoiceTaken.textContent = `Player choice: ${humanChoice}`;
+    computerChoiceTaken.textContent = `Computer choice: ${computerChoice}`;
+
     let oneRound = playRound(humanChoice, computerChoice);
     
     if (oneRound === "you won"){
-        console.log("you won");
         humanScore = humanScore + 1;
+        gameInfo.textContent = "The Player has won this round!";
     }
     else if (oneRound === "you lose"){
-        console.log("you lose");
         computerScore = computerScore + 1;
+        gameInfo.textContent = "The Computer has won this round!";
     }
     else if (oneRound === "equal"){ 
-        console.log("equal");
+        gameInfo.textContent = "Equal between the participants!";
     }
-    else {} 
-    console.log(`This is your score: ${humanScore}`);
-    console.log(`This is the computer score: ${computerScore}`);
+    else {}; 
 
     if( (humanScore === 5) && (computerScore < 5)){
-    console.log("You win the game!");
     humanScore = 0;
     computerScore = 0;
+    gameInfo.textContent = "The Player has won the game!"
     }                   
     else if( (humanScore < 5) && (computerScore === 5) ){
-    console.log("You have lost the game!")
     humanScore = 0;
     computerScore = 0;
+    gameInfo.textContent = "The Computer has won the game!";
     }
-    else if( (humanScore === computerScore) &&
-    (humanScore === 5) &&
-    ( computerScore === 5) ){
-    console.log("It is equal between the 2 participants!")
-    }
-    else{}
+    else{};
 
-
-    humanScoreBoard.textContent = `Player score is: ${humanScore}`;
+    humanScoreBoard.textContent = `Player score: ${humanScore}`;
+    computerScoreBoard.textContent = `Computer score: ${computerScore}`;
 
     } );
  
 function getComputerChoice(pc_choice) {
     if (pc_choice <= 33) {
-        //console.log("rock");
         return "rock";
     }
     else if ((pc_choice <= 66) && (pc_choice > 33)) { 
-        //console.log("paper");
         return "paper";
     }
     else if ((pc_choice <= 100) && (pc_choice > 66)) {
-        //console.log("scissor");
         return "scissor";
-    } 
+    }; 
 };                  
 
 function playRound(humanChoice, computerChoice) {
     if(humanChoice === computerChoice){
-        //console.log("equal"); 
         return "equal";
     }
     else if ((humanChoice === "rock") && (computerChoice === "scissor")){   
-        //console.log("you won");
         return "you won";
     }
     else if ((humanChoice === "rock") && (computerChoice === "paper")){
-        //console.log("you lose");
         return "you lose";
     }  
     else if ((humanChoice === "paper") && (computerChoice) === "rock"){
-        //console.log("you won");
         return "you won";
     }
     else if ((humanChoice === "paper") && (computerChoice) === "scissor"){
-        //console.log("you lose");
         return "you lose";
     }
     else if ((humanChoice === "scissor") && (computerChoice) === "rock"){
-        //console.log("you lose");
         return "you lose";
     }
     else if ((humanChoice === "scissor") && (computerChoice) === "paper"){
-        //console.log("you won");
         return "you won"
     }
     else {
-        //console.log("invalid input");
         return "invalid input";
-    }
-}
+    };
+};
 
   
